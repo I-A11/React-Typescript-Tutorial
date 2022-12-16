51ac54dc8eb4c5c49ad5eec3d7be05da81579ed4
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-16 19:36:11 +1100

    Use typescript with useState hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import React from "react";
 import "./App.css";
-import Button from "./components/Button";
-import Container from "./components/Container";
-import Input from "./components/Input";
 
 function App() {
-  return (
-    <div className='App'>
-      <Container
-        styles={{ border: "1px solid black", padding: "1rem", width: "90%" }}
-      />
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
