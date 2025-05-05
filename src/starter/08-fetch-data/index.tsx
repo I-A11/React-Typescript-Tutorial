b32011feece139aef7f30af867076059df804137
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2025-05-05 10:19:12 +1000

    Create fetchData function to get tours from api.

--- src/starter/08-fetch-data/index.tsx
+++ src/starter/08-fetch-data/index.tsx
@@ -1,4 +1,40 @@
+import { useState, useEffect } from "react";
+const url = "https://www.course-api.com/react-tours-project";
+
 function Component() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch tours...`);
+      }
+      const rawData = await response.json();
+      console.log(rawData);
+    } catch (error) {
+      const message =
+        error instanceof Error ? error.message : "there is an error...";
+      setIsError(message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    <h3>Error {isError}</h3>;
+  }
+
   return (
     <div>
       <h2>React & Typescript</h2>
